5e2f9e01fc0c4c4e18e91ffcd8b58e082c6fdd1b
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2025-06-25 10:14:03 +0000

    feat: enable code splitting and change output format to esm

--- build.ts
+++ build.ts
@@ -3,22 +3,46 @@ import { Glob } from "glob";
 import esbuild from "esbuild";
 import { copy } from "jsr:@std/fs";
 
+const srcDir = "src";
+const distDir = "dist";
+
 await esbuild.build({
   sourcemap: false,
   platform: "browser",
-  entryPoints: ["src/background.ts", "src/content.ts", "src/popup.ts"],
-  outdir: "dist/",
+  entryPoints: [
+    `${srcDir}/background.ts`,
+    `${srcDir}/content.ts`,
+    `${srcDir}/popup.ts`,
+  ],
+  outdir: distDir,
+  bundle: true,
+  legalComments: "eof",
+  splitting: true,
+  format: "esm",
+  chunkNames: "deps",
+});
+
+// content scripts are not supported as esm modules
+await esbuild.build({
+  sourcemap: false,
+  platform: "browser",
+  entryPoints: [`${srcDir}/content.ts`],
+  outdir: distDir,
   bundle: true,
   legalComments: "eof",
 });
 
-const assetStream = new Glob(["src/*.html", "src/*.css", "src/*.json"], {
+const assetStream = new Glob([
+  `${srcDir}/*.html`,
+  `${srcDir}/*.css`,
+  `${srcDir}/*.json`,
+], {
   withFileTypes: true,
 });
 assetStream.stream().on("data", async (path) => {
-  await Deno.copyFile(path.fullpath(), "dist/" + path.name);
+  await Deno.copyFile(path.fullpath(), `${distDir}/${path.name}`);
 });
 
-copy("src/images/", "dist/images/", { overwrite: true });
+copy(`${srcDir}/images/`, `${distDir}/images/`, { overwrite: true });
 
 await esbuild.stop();
